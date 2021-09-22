93690f8862c904e9d153e79ebbb5abb4f4ce1a43
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2021-09-22 18:14:36 +0000

    Finally solved mechanic behind the winning conditions of the elements. - implemented addressing system that chooses the right word for the right element which wins over the other

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import random
 from os import system, name
 from time import sleep
 
+
 def clear():
-  
+
     # for windows
     if name == 'nt':
         _ = system('cls')
-  
+
     # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
 
+
+element_word = {
+    rock: "rock",
+    paper: "paper",
+    scissors: "scissors",
+    spock: "spock",
+    lizard: "lizard",
+}
 
 rock_win = [scissors, lizard]
 paper_win = [spock, rock]
@@ -22,16 +31,18 @@ lizard_win = [paper, spock]
 player_score = 0
 computer_score = 0
 
-element_win = [ 
-  [rock, ["breaks", "crushes"]],
-  [paper, ["disproves", "covers"]],
-  [scissors, ["slices", "cuts"]],
-  [spock, ["vaporises", "crushes"]],
-  [lizard, ["eats", "poisons"]]
-]
+element_win = [[rock, ["breaks", "crushes"]], [paper, ["disproves", "covers"]],
+               [scissors, ["slices",
+                           "cuts"]], [spock, ["vaporises", "crushes"]],
+               [lizard, ["eats", "poisons"]]]
 
-gesture = [[rock, rock_win], [paper, paper_win], [scissors, scissors_win],
-           [spock, spock_win], [lizard, lizard_win]]
+gesture = [
+    [rock, rock_win],
+    [paper, paper_win],
+    [scissors, scissors_win],
+    [spock, spock_win],
+    [lizard, lizard_win],
+]
 
 game = "y"
 
@@ -51,20 +62,13 @@ while (game == "y"):
 
   Your choice: '''))
     clear()
-    
+
     # Simplifying addressing position of choices and winning conditions
     player_hand = gesture[player][0]
     computer_hand = gesture[computer][0]
     c_loser_list = gesture[player][1]
     p_loser_list = gesture[computer][1]
-    
 
-
-    player_wins = element_win[player][1][c_loser_list.index(computer_hand)]
-    # computer_wins = element_win[computer][1][p_loser_list.index(player_hand)]
-    print(player_wins)
-    # print(computer_wins)
-    break
 
     if player >= 0 and player <= 4:
         print(f''' You play 
@@ -77,10 +81,23 @@ while (game == "y"):
         if player_hand == computer_hand:
             print("It is a draw!\n")
         elif computer_hand in c_loser_list:
-            
+            defeats = element_win[player][1][c_loser_list.index(computer_hand)]
+
+            print(
+                f"'{element_word[player_hand]} {defeats} {element_word[computer_hand]}'"
+            )
+
             print("Player wins!\n")
+
             player_score += 1
+
         else:
+            defeats = element_win[computer][1][p_loser_list.index(player_hand)]
+
+            print(
+                f"'{element_word[computer_hand]} {defeats} {element_word[player_hand]}'"
+            )
+
             print("Computer wins!\n")
             computer_score += 1
     else:
